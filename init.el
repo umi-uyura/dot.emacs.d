dfe3dd87573bd5bdca593eb05fb1866e3499c057
Umi Uyura <umiuyura@gmail.com>
2015-05-12 10:14:26 +0900

    JSX(React)用flycheckの設定を追加

--- init.el
+++ init.el
@@ -1089,12 +1089,28 @@
 ;;;; flycheck settings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 
+(require 'flycheck)
+
 (add-hook 'after-init-hook 'global-flycheck-mode)
 
 ;; flycheck-pos-tip.el
 (eval-after-load 'flycheck
   '(custom-set-variables
     '(flycheck-display-errors-function 'flycheck-pos-tip-error-messages)))
+
+;; flycheck to JSX for web-mode
+(flycheck-define-checker jsxhint-checker
+  "A JSX syntax and style checker based on JSXHint."
+  :command ("jsxhint" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  :modes (web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (equal web-mode-content-type "jsx")
+              ;; enable flycheck
+              (flycheck-select-checker 'jsxhint-checker)
+              (flycheck-mode))))
 
 
 ;;;; howm settings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

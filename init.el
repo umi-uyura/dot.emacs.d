c258440f9e588c1408d53ee7eed5729de76ded81
Umi Uyura <umiuyura@gmail.com>
2015-08-19 15:07:15 +0900

    PHPをweb-modeで開く設定を追加

--- init.el
+++ init.el
@@ -896,6 +896,7 @@
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))

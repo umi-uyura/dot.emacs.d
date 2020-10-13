edf87c8b93de7284cbaca02f645dd8d3716cf5e3
Umi Uyura <umiuyura@gmail.com>
2020-10-13 11:16:15 +0900

    カスタマイズファイル追加

---
+++ custom.el
@@ -0,0 +1,41 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ac-auto-start 3)
+ '(ac-comphist-file (concat DOT_EMACS_VAR "/ac-comphist.dat"))
+ '(ac-quick-help-delay 1.0)
+ '(ac-use-fuzzy t)
+ '(coffee-args-compile '("-c" "--bare"))
+ '(coffee-tab-width 2)
+ '(flycheck-disabled-checkers '(javascript-jshint javascript-jscs))
+ '(global-emojify-mode t)
+ '(json-reformat:indent-width 2)
+ '(package-selected-packages
+   '(textile-mode emojify flycheck ghub git-commit helm helm-core jedi-core magit-popup markdown-mode package-build projectile tern with-editor yasnippet yaml-mode writeroom-mode wgrep-pt wgrep-helm web-mode web-beautify use-package undo-tree tern-auto-complete sudo-ext stylus-mode smex smartparens smart-cursor-color shell-command sass-mode ruby-end rg restclient relative-buffers recentf-ext rainbow-mode rainbow-delimiters python-mode pug-mode pt projectile-ripgrep prodigy popwin play-routes-mode pallet osx-dictionary open-junk-file nyan-mode multiple-cursors moe-theme markdown-toc magit less-css-mode json-mode js-doc jedi jaword japanese-holidays jade-mode inf-ruby idle-highlight-mode htmlize howm grep-a-lot gradle-mode google-translate gmail-message-mode git-modes flycheck-pos-tip flycheck-cask expand-region exec-path-from-shell ensime emoji-cheat-sheet-plus edit-server dsvn drag-stuff csv-mode color-moccur coffee-mode cask-mode ample-zen-theme alect-themes ac-js2 ac-html ac-emoji))
+ '(projectile-cache-file (concat PROJECTILE_TEMP "/projectile.cache"))
+ '(projectile-known-projects-file (concat PROJECTILE_TEMP "/projectile-bookmarks.eld"))
+ '(py-indent-offset 2)
+ '(recentf-exclude '("^/[^/:]+:" "!semantic.cache$"))
+ '(recentf-max-saved-items 3000)
+ '(recentf-save-file (concat DOT_EMACS_VAR "/recentf"))
+ '(url-cache-directory (concat DOT_EMACS_VAR "/url/cache"))
+ '(url-configuration-directory (concat DOT_EMACS_VAR "/url"))
+ '(url-cookie-file (concat DOT_EMACS_VAR "/url/cookies")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(nxml-attribute-value-delimiter-face ((t (:foreground "magenta"))))
+ '(nxml-comment-content-face ((t (:foreground "darkgreen"))))
+ '(nxml-comment-delimiter-face ((t (:foreground "darkgreen"))))
+ '(nxml-delimited-data-face ((t (:foreground "magenta"))))
+ '(nxml-delimiter-face ((t (:foreground "blue"))))
+ '(nxml-element-colon-face ((t (:foreground "black"))))
+ '(nxml-element-local-name-face ((t (:inherit nxml-name-face :foreground "blue"))))
+ '(nxml-name-face ((t (:foreground "purple"))))
+ '(nxml-ref-face ((t (:foreground "red"))))
+ '(nxml-tag-slash-face ((t (:inherit nxml-name-face :foreground "blue" :bold t))))
+ '(nxml-text-face ((t (:foreground "black" :background "#e5e5e5")))))
